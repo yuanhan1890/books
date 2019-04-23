b547d74b9b9ecf7a2a3be4fd3dbf915600f86aa8
yuanhan1890 <yuanpy2009@qq.com>
2019-04-23 20:40:00 +0800

    add row reduction methods

--- src/matrix.ts
+++ src/matrix.ts
@@ -12,6 +12,44 @@ export class Matrix {
   ) {
   }
 
+  public swapRows(i: number, j: number) {
+    let temp: number;
+    const baseI: number = this.columns * i;
+    const baseJ: number = this.columns * j;
+    for (let c = 0; c < this.columns; c += 1) {
+      temp = this.data[baseI + c];
+      this.data[baseI + c] = this.data[baseJ + c];
+      this.data[baseJ + c] = temp;
+    }
+  }
+
+  public getRow(i: number) {
+    return this.data.slice(i * this.columns, (i + 1) * this.columns);
+  }
+
+  public getCol(i: number) {
+    const data: number[] = [];
+    for (let r = 0; r < this.rows; r += 1) {
+      data.push(this.data[r * this.columns + i]);
+    }
+    return data;
+  }
+
+  public multiRow(i: number, a: number) {
+    const base = i * this.columns;
+    for (let c = 0; c < this.columns; c += 1) {
+      this.data[base + c] *= a;
+    }
+  }
+
+  public multiThenAdd(i: number, a: number, j: number) {
+    const baseI = i * this.columns;
+    const baseJ = j * this.columns;
+    for (let c = 0; c < this.columns; c += 1) {
+      this.data[baseJ + c] += (this.data[baseI + c] * a);
+    }
+  }
+
   public toString() {
     const data: string[] = this.data.map((num) => `${num % 1 === 0 ? num : num.toFixed(2)}`);
     const maxLen = Math.max(...data.map((s) => s.length)) + 2;
